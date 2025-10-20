2f492e10247d667314d1d6ebf0250e0488796d47
Yiannis <ioannis.roussos@hotmail.com>
2025-10-20 13:04:39 +0300

    still not quite

--- index.js
+++ index.js
@@ -27,15 +27,15 @@ async function main() {
         return `<div class="movie-card__container">
         <div class="movie-card">
                       <h3>${movie.Title} </h3>
-                      <img src="${movie.Poster}"alt="${movie.Title}" />
+                      <img src= "${movie.Poster || "./assets/noimage.jpg"} "alt="${movie.Title}" />
                       <p><b>Type:</b> ${movie.Type}</p>
                       <p><b>Year:</b> ${movie.Year} </p>
                       <p class="imdb__link"><b>imdbID:</b>${movie.imdbID} <a href="https://www.imdb.com/title/${movie.imdbID}/" target="_blank"></p>
                       
                  </div>
                     </div>`
-                    if (!movie.Poster) then (movie.Poster = './assets/no image.jpg')                    }
             console.log(data.Search)
+    }
 
             
             
